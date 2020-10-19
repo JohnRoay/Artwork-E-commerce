c2e9048b0741b814a09de7624b6f6273b138ed07
Isabellanu <rumeshtharindu2020@gmail.com>
2020-10-19 11:06:07 +0530

    add bootsrap nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './component/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+       <Header />
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Navbar,Nav} from 'react-bootstrap'
+function Header() {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+      <Nav.Link eventKey={2} href="#memes">
+        Dank memes
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar> 
+        </div>
+    )
+}
+
+export default Header
